41cf2d74878e4ecd63ff6b6ee7dd088fd72ad0af
FelipeArredond <pipechiqui77@gmail.com>
2022-05-23 18:44:18 -0500

    View jobs applied done

--- src/components/mainpage/MainPage.jsx
+++ src/components/mainpage/MainPage.jsx
@@ -7,6 +7,8 @@ import { EmployerMenu } from '../employer/EmployerMenu';
 export function MainPage(){
 
     const [jobsList, setJobsList] = useState([])
+    const [jobsApplied, setJobsApplied] = useState([])
+    const [jobslisted, setJobslisted] = useState(true)
 
     const context = useContext(authContext)
 
@@ -21,6 +23,20 @@ export function MainPage(){
         })
         const data = await response.json()
         setJobsList(data)
+    }
+
+    const getJobsApplied = async () =>{
+        const token = localStorage.getItem("token")
+        const response = await fetch("https://backendnodejstzuzulcode.uw.r.appspot.com/api/jobs/me", {
+            method: 'POST',
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+ token
+            }
+        })
+        const data = await response.json()
+        setJobsApplied(data)
+        setJobslisted(!jobslisted);
     }
 
     useEffect(() => {
@@ -39,10 +55,24 @@ export function MainPage(){
         )
     })
 
+    const mapJobsApplied = jobsApplied.map(job => {
+        return(
+            <JobItem tittle={job.title}
+            country={job.location.country}
+            province={job.location.province}
+            description={job.description}
+            salary={job.salary}
+            id={job._id}
+            ></JobItem>
+        )
+    })
+
     return(
         <>
             <h1 className='welcomeUser'>{context.auth.logged&&'¡Bienvenido ' + context.auth.name + '!'}</h1>
-            {context.auth.role==='applicant'&&mapJobsList}
+            {context.auth.role==='applicant'&&<h1 className='welcomeUser' onClick={getJobsApplied}>Ver empleos aplicados!</h1>}
+            {context.auth.role==='applicant'&&!jobslisted&&mapJobsApplied}
+            {context.auth.role==='applicant'&&jobslisted&&mapJobsList}
             {context.auth.role==='employer'&&<EmployerMenu></EmployerMenu>}
         </>
     );
